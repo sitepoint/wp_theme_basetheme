cdca50d9a35723f4ed0b1f4f12741433c4efa0d2
Anthony Hortin <anthony@maddisondesigns.com>
2016-09-06 19:28:15 +1000

    Display comments on Pages only if there are any and if comments aren't disabled

--- page.php
+++ page.php
@@ -24,6 +24,13 @@ get_header(); ?>
 				<?php // Start the Loop ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<?php get_template_part( 'content', 'page' ); ?>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) {
+						comments_template( '', true );
+					}
+					?>
 				<?php endwhile; ?>
 
 			<?php endif; // end have_posts() check ?>
