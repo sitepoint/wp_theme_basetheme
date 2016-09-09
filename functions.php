bbd475b5d6f2a76f9b0272c998da4e77bdd6c840
Anthony Hortin <anthony@maddisondesigns.com>
2016-09-09 11:17:38 +1000

    Include script to invoke our button toggle for the main navigation

--- functions.php
+++ functions.php
@@ -388,9 +388,9 @@ if ( ! function_exists( 'sitepoint_scripts_styles' ) ) {
 			);
 		}
 
-		// Include this script to envoke a button toggle for the main navigation menu on small screens
-		//wp_register_script( 'small-menu', trailingslashit( get_template_directory_uri() ) . 'js/small-menu.js', array( 'jquery' ), '20130130', true );
-		//wp_enqueue_script( 'small-menu' );
+		// Load our script that envokes a button toggle for the main navigation menu on small screens
+		wp_register_script( 'small-menu', trailingslashit( get_template_directory_uri() ) . 'js/small-menu.js', array( 'jquery' ), '1.0.0', true );
+		wp_enqueue_script( 'small-menu' );
 
 	}
 }
