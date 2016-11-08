334d6a0673ba4d57c2fbdf16ae5b1a2a6e9992bc
Leo Anthony Capinpin <lcapinpin@Zigzags-MacBook-Air-3.local>
2016-11-08 09:12:56 +0800

    Updated pagination call function

--- category.php
+++ category.php
@@ -28,7 +28,7 @@ get_header(); ?>
 					<?php get_template_part( 'content', get_post_format() ); ?>
 				<?php endwhile; ?>
 
-				<?php sitepointbasetheme_content_nav( 'nav-below' ); ?>
+				<?php the_posts_pagination( 'nav-below' ); ?>
 
 			<?php else : ?>
 
